e2a46ecfd820a27524657b6d6eacb82a7fe6a592
madisonweiss <madison.l.weiss@icloud.com>
2025-02-09 04:47:48 -0500

    set up camera firebase and groups view

---
+++ snipe/CameraFunctions/CameraFunctionsController.swift
@@ -0,0 +1,29 @@
+//
+//  CameraFunctionsController.swift
+//  snipe
+//
+//  Created by Madison Weiss on 2/9/25.
+//
+
+import UIKit
+
+class CameraFunctionsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
