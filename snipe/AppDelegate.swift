7f2ef34f84a9d134ea4ba7c69560697582d63c0c
Evelyn Xiao <evelvnxiao@gmail.com>
2025-02-08 20:01:21 -0500

    login, create, and logout functionalities

--- snipe/AppDelegate.swift
+++ snipe/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseAuth
 
 
 @main
@@ -16,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        //FirebaseApp.configure()
         FirebaseApp.configure()
         return true
     }
